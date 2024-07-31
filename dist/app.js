7a63e023551adc90d9d9e9e7c5e6473cc7c5e9bd
Victor Peinado <vpeinado263@gmail.com>
2024-07-31 15:29:41 -0300

    mas cambios

--- dist/app.js
+++ dist/app.js
@@ -5,13 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
-const SessionConfig_1 = require("./config/SessionConfig");
 const initDB_1 = __importDefault(require("./scripts/initDB"));
 const AppRoute_1 = __importDefault(require("./routes/app/AppRoute"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
-app.use(SessionConfig_1.sessionConfig);
 (0, initDB_1.default)();
 app.use((0, cors_1.default)({
     origin: ['http://localhost:3000', 'https://tienda-x--swart.vercel.app'],
