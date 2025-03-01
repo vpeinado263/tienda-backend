d2bb051c8fb1d3bf337d6b4b496072651d2dc702
Victor Peinado <vpeinado263@gmail.com>
2025-02-28 22:37:27 -0300

    Simplificar estructura de back-end

--- src/routes/app/AppRoute.ts
+++ src/routes/app/AppRoute.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express'; 
 import productRoutes from '../productRoute/ProductRoute';
-import userRoutes from '../userRoute/UserRoute';
 
 const AppRouter = Router(); 
 
 AppRouter.use('/products', productRoutes);
-AppRouter.use('/users', userRoutes);
 
 export default AppRouter;
